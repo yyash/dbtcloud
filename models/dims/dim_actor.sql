aa48fbfd4147dd340a326561c35fc7c30f52f164
Yash Bhatia <yash.bhatia.2022@anderson.ucla.edu>
2022-02-23 01:52:54 +0000

    dim_customer created

--- models/dims/dim_actor.sql
+++ models/dims/dim_actor.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='table') }}
 
 select 
+{{ dbt_utils.surrogate_key(['stg_actor.actor_id']) }} ACTOR_KEY,
 ACTOR_ID AS ID,
 --CONCAT_WS(' ', LAST_NAME, FIRST_NAME) as FULL_NAME
 CONCAT(FIRST_NAME,' ',LAST_NAME) as FULL_NAME
